435984c87f17e9a3bcdb09085862021a9bef1f1a
cspro <michael.daross@pearson.com>
2015-07-22 15:59:15 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			build: {
+				src: ["build"]
+			}
+		},
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded',
+					sourcemap: 'auto'
+				},
+				files: {
+					'src/css/main.css': 'src/sass/main.scss'
+				}
+			}
+		},
+		copy: {
+			build: {
+				cwd: 'src',
+				src: ['**/*', '!**/sass/**', '!**/config.rb'],
+				dest: 'build',
+				expand: true
+			}
+		},
+		watch: {
+			files: ['src/**/*', '!src/css/main.css**'], //Don't watch css, because the sass task will modify it --> infinite loop
+			tasks: ['default']
+		}
+	});
+	
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	
+	grunt.registerTask('default', ['clean', 'sass', 'copy']);
+};
---
+++ build/js/app.js
@@ -0,0 +1,86 @@
+(function() {
+	var app = angular.module('exampleApp', ['ngRoute', 'ngAnimate']);
+	
+	app.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: 'partials/main.tpl.html',
+				controller: 'MainCtrl'
+			}).
+			otherwise({
+				redirectTo: '/'
+			});
+	}]);
+	
+	app.run(function($rootScope, $location) {
+		$rootScope.$on('$routeChangeStart', function(event, next, current) {
+			// event.preventDefault();
+			if (current) {
+				console.log(current.controller + " --> " + next.controller);
+			} else {
+				console.log(next.controller);
+			}
+		});
+		$rootScope.$on('$routeChangeSuccess', function(event, next, current) {
+			console.log("$routeChangeSuccess: " + event);
+		});
+		$rootScope.$on('$locationChangeStart', function(event, toState) {
+			console.log("$locationChangeStart: " + event);
+		});
+	});
+	
+	app.service('$dataService', function() {
+		this.getData = function() {
+			if (!this.data) {
+				this.dataMap = {};
+				angular.forEach(rawData, function(comp, key) {
+				});
+			}
+			return this.data;
+		};
+		this.getSomeData = function(someId) {
+			this.getData();
+			return this.dataMap[someId];
+		};
+	});
+	
+	app.controller('RootCtrl', function($rootScope, $timeout, $location) {
+		$scope = $rootScope;
+		$scope.initialized = false;
+		
+		this.onResetClick = function(event) {
+			event.preventDefault();
+			$timeout(function() {
+				$location.path("/somepath");
+			}, 10);
+		};
+		
+		$timeout(function() {
+			$scope.initialized = true;
+		}, 200);
+	});
+	
+	app.controller('MainCtrl', function($rootScope, $scope, $location, $timeout, $dataService) {
+		
+		this.msg = "Competency message";
+		
+		this.comps = $dataService.getData();
+		
+		this.onLinkClick = function(event, somedata) {
+			event.preventDefault();
+			$timeout(function() {
+				$location.path("/somepath/" + somedata.id);
+			}, 10);
+		};
+		
+	});
+	
+	app.controller('SubCtrl', function($scope, $routeParams, $rootScope, $location, $timeout, $dataService) {
+	
+		var compId = $routeParams.someId;
+		var comp = $dataService.getData(someId);
+	});
+
+	
+		
+})();
